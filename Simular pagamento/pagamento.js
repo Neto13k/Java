b1a79d395e207f5b1e0903daf9384f456b38d37d
Jose Neto <josenetto13k@hotmail.com>
2025-08-21 21:42:53 -0300

    Atualização pagamento

--- Simular pagamento/pagamento.js
+++ Simular pagamento/pagamento.js
@@ -1,10 +1,14 @@
-function Pagamento(){
+async function Pagamento(){
     const pagamento = document.getElementById("pagamento").value;
     const resultado = document.getElementById("resultado");
 
+console.log("Estamos processando o pagamento...")
+
+await new Promise(resolve => setTimeout(resolve, 5000));
     if (pagamento > 1000) {
         resultado.innerHTML = "Saldo insuficiente";
     } else {
         resultado.innerHTML = "Pagamento Aprovado!";
     }
 }
+
